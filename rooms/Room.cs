5434ecc8c85a5edd8737d98ee0256fad6eb50772
Chris Chenery <Cecer1@cecer1.com>
2012-03-26 05:11:40 +0100

    Added basic Room data access.

---
+++ rooms/Room.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IHI.Database
+{
+    public class Room
+    {
+        public virtual int room_id
+        {
+            get;
+            set;
+        }
+        public virtual int owner_id
+        {
+            get;
+            set;
+        }
+        public virtual string name
+        {
+            get;
+            set;
+        }
+        public virtual string description
+        {
+            get;
+            set;
+        }
+        public virtual string category_id
+        {
+            get;
+            set;
+        }
+    }
+}
